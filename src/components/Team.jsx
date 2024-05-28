39ac20ed8561835a3297dc721a934073faef885b
Sammienator <sammugo18@gmail.com>
2024-05-28 23:41:17 +0300

    changes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import test1 from '../assets/test1.jpg'
 import test2 from '../assets/test2.jpg'
 
 const Team = () => {
+
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
@@ -11,13 +18,13 @@ const Team = () => {
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
     </div>
     <div class="flex flex-wrap -m-4">
-      <div class="p-4 lg:w-1/4 md:w-1/2">
+      <div data-aos="fade-up" data-aos-duration="2000" class="p-4 lg:w-1/3 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={test1} />
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+            <h2 class="title-font font-medium text-lg text-gray-900">Reagan "The Rapper" Kamau</h2>
+            <h3 class="text-gray-500 mb-3">Lead Vocalist: </h3>
+            <p class="mb-4">Regan brings unparalleled energy and charisma to the stage, captivating audiences with powerful vocals and dynamic rap performances. As the frontperson, Reagan's unique voice defines the band's sound and engages fans from the first note.</p>
             <span class="inline-flex">
               <a href='#b' class="text-gray-500">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -38,13 +45,13 @@ const Team = () => {
           </div>
         </div>
       </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
+      <div data-aos="fade-left" data-aos-duration="2000" class="p-4 lg:w-1/3 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"  src={test2} />
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+            <h2 class="title-font font-medium text-lg text-gray-900">Mdau "The Maestro" Two</h2>
+            <h3 class="text-gray-500 mb-3"> Lead Guitarist: </h3>
+            <p class="mb-4">Mdau is the musical genius behind the band’s electrifying guitar riffs and solos. With a talent for crafting intricate melodies and a knack for improvisation, Mdau's guitar work adds depth and excitement to every song.</p>
             <span class="inline-flex">
               <a href='#b' class="text-gray-500">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -65,13 +72,13 @@ const Team = () => {
           </div>
         </div>
       </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
+      <div data-aos="fade-left" data-aos-duration="2000" class="p-4 lg:w-1/3 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={test1} />
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+            <h2 class="title-font font-medium text-lg text-gray-900">Taylor "The Beat" Davis</h2>
+            <h3 class="text-gray-500 mb-3">Drummer </h3>
+            <p class="mb-4">Taylor drives the band's rhythm with precision and flair, delivering powerful drum beats that keep the tempo tight and the crowd moving. With an exceptional sense of timing and creativity, Taylor's drumming is the heartbeat of the band.</p>
             <span class="inline-flex">
               <a href='#b' class="text-gray-500">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -92,33 +99,7 @@ const Team = () => {
           </div>
         </div>
       </div>
-      <div class="p-4 lg:w-1/4 md:w-1/2">
-        <div class="h-full flex flex-col items-center text-center">
-          <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={test2} />
-          <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
-            <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
-            <span class="inline-flex">
-              <a href='#b' class="text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a href='#b' class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a href='#b' class="ml-2 text-gray-500">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                </svg>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
+  
     </div>
   </div>
 </section>
